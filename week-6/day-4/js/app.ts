f0618c3f3d411cbe6dae677f0c25ecf24bf186d6
deekshithm-vonnue <deekshith.m@vonnue.com>
2026-08-13 09:54:32 +0530

    feat: completed middleware

--- week-6/day-4/js/app.ts
+++ week-6/day-4/js/app.ts
@@ -8,4 +8,18 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.use("/tasks", router);
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "Not found",
+  });
+});
+
+app.use((err: any, req: any, res: any, next: any) => {
+  res.status(500).json({
+    message: "Internal Server Error",
+  });
+});
+
 export default app;
